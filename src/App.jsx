f414a76cd05d779e35811cff9303398901e43985
david <davidtucu@hotmail.com>
2022-09-25 18:27:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container, ModalBody } from 'react-bootstrap'
+function App() {
+
+  return (
+    <div className="App">
+     <header className="py-5">
+            <h1>Buscador de Bebidas</h1>
+          </header>
+    </div>
+  )
+}
+
+export default App
